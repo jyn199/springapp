844faca600a8e71eb4202984c3356743d09704aa
niannian199 <niannian199@sina.com>
2015-06-18 12:37:19 +0800

    'add_index_jsp'

--- src/main/java/com/app/demo/AppController.java
+++ src/main/java/com/app/demo/AppController.java
@@ -8,12 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/app")
+@RequestMapping("/")
 public class AppController {
+	
+	@RequestMapping(value="app")
+	public ModelAndView appIndex(HttpServletRequest request, HttpServletResponse response){
+		ModelAndView mav = new ModelAndView("/app/index");
+		return mav;
+	}
+	
 	
 	@RequestMapping(value="")
 	public ModelAndView index(HttpServletRequest request, HttpServletResponse response){
-		ModelAndView mav = new ModelAndView("/app/index");
+		ModelAndView mav = new ModelAndView("/index");
 		return mav;
 	}
 }
